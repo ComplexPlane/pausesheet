5c5756e783b4139e4a8a62f358f9b55c99dd1972
ComplexPlane <complexplanegames@gmail.com>
2021-01-01 15:21:57 -0500

    Perform all (most?) reads before all writes, keeping caches fresh
    Seems to improve perf a lot

--- code.ts
+++ code.ts
@@ -1,5 +1,16 @@
 import SS = GoogleAppsScript.Spreadsheet;
 
+interface Context {
+    range: SS.Range;
+    values: any[][];
+    bgColors: any[][];
+    fontSizes: any[][];
+    fontColors: any[][];
+    fontFamilies: any[][];
+    fontStyles: any[][];
+    fontWeights: any[][];
+}
+
 function onOpen() {
     const ui = SpreadsheetApp.getUi();
     // Or DocumentApp or FormApp.
@@ -19,98 +30,99 @@ function genRangeArray(range: SS.Range): any[][] {
 function formatStageTable() {
     const sheet = SpreadsheetApp.getActive();
     const range = sheet.getActiveSheet().getActiveRange();
+    const ctx: Context = { 
+        range: range,
+        values: range.getValues(),
+        bgColors: range.getBackgrounds(),
+        fontSizes: range.getFontSizes(),
+        fontColors: range.getFontColors(),
+        fontFamilies: range.getFontFamilies(),
+        fontStyles: range.getFontStyles(),
+        fontWeights: range.getFontWeights(),
+    };
+    // After this point, zero additional reads from the sheet are allowed
+
+    doMerges(ctx);
+    setBgColor(ctx);
+    setFontStuff(ctx);
+    // setBorders(range); // TODO optimize
 
     range.setNumberFormat('@'); // '@' means plain text for some reason...
     // For more info: https://www.blackcj.com/blog/2015/05/18/cell-number-formatting-with-google-apps-script/
-
-    setBgColor(range);
-    setFontStuff(range);
-    // setBorders(range); // TODO optimize
-    doMerges(range);
 };
 
-function setFontStuff(range: SS.Range) {
-    const fontSizes = genRangeArray(range);
-    const fontColors = genRangeArray(range);
-    const fontFamilies = genRangeArray(range);
-    const fontStyles = genRangeArray(range);
-    const fontWeights = genRangeArray(range);
-
-    for (let row = 0; row < range.getNumRows(); row++) {
-        for (let col = 0; col < range.getNumColumns(); col++) {
-            fontColors[row][col] = 'black';
-            fontStyles[row][col] = 'normal'; // TODO adjustment description should be italic
+function setFontStuff(ctx: Context) {
+    for (let row = 0; row < ctx.range.getNumRows(); row++) {
+        for (let col = 0; col < ctx.range.getNumColumns(); col++) {
+            ctx.fontColors[row][col] = 'black';
+            ctx.fontStyles[row][col] = 'normal'; // TODO adjustment description should be italic
 
             if (col == 0) {
                 // Stage name
-                fontSizes[row][col] = 10;
-                fontFamilies[row][col] = 'Arial';
-                fontWeights[row][col] = 'bold';
+                ctx.fontSizes[row][col] = 10;
+                ctx.fontFamilies[row][col] = 'Arial';
+                ctx.fontWeights[row][col] = 'bold';
 
             } else {
                 // Frames
                 if (col % 2 == 1) {
                     // Input column
-                    fontSizes[row][col] = 15;
-                    fontFamilies[row][col] = 'Calibri';
-                    fontWeights[row][col] = 'bold';
+                    ctx.fontSizes[row][col] = 15;
+                    ctx.fontFamilies[row][col] = 'Calibri';
+                    ctx.fontWeights[row][col] = 'bold';
                 } else {
                     // Frame column
-                    fontSizes[row][col] = 11;
-                    fontFamilies[row][col] = 'arial';
-                    fontWeights[row][col] = 'normal';
+                    ctx.fontSizes[row][col] = 11;
+                    ctx.fontFamilies[row][col] = 'arial';
+                    ctx.fontWeights[row][col] = 'normal';
                 }
             }
         }
     }
 
-    range.setFontSizes(fontSizes);
-    range.setFontColors(fontColors);
-    range.setFontFamilies(fontFamilies);
-    range.setFontStyles(fontStyles);
-    range.setFontWeights(fontWeights);
+    ctx.range.setFontSizes(ctx.fontSizes);
+    ctx.range.setFontColors(ctx.fontColors);
+    ctx.range.setFontFamilies(ctx.fontFamilies);
+    ctx.range.setFontStyles(ctx.fontStyles);
+    ctx.range.setFontWeights(ctx.fontWeights);
 }
 
-function setBorders(range: SS.Range) {
+function setBorders(ctx: Context) {
     // Clear borders initially
-    range.setBorder(false, false, false, false, false, false);
+    ctx.range.setBorder(false, false, false, false, false, false);
 
-    const values = range.getValues();
     const nonEmptyCellLocs = [];
-    for (let row = 1; row <= range.getNumRows(); row++) {
-        for (let col = 1; col <= range.getNumColumns(); col++) {
-            if (values[row - 1][col - 1] !== '') {
-                const absRow = row + range.getRow() - 1;
-                const absCol = col + range.getColumn() - 1;
+    for (let row = 1; row <= ctx.range.getNumRows(); row++) {
+        for (let col = 1; col <= ctx.range.getNumColumns(); col++) {
+            if (ctx.values[row - 1][col - 1] !== '') {
+                const absRow = row + ctx.range.getRow() - 1;
+                const absCol = col + ctx.range.getColumn() - 1;
                 nonEmptyCellLocs.push(`R${absRow}C${absCol}`);
             }
         }
     }
 
-    const rangeList = range.getSheet().getRangeList(nonEmptyCellLocs); // This is slow...
+    const rangeList = ctx.range.getSheet().getRangeList(nonEmptyCellLocs); // This is slow... should this be done during "read" phase?
     rangeList.setBorder(true, true, true, true, true, true);
 }
 
-function setBgColor(range: SS.Range) {
-    const backgrounds = genRangeArray(range);
-
+function setBgColor(ctx: Context) {
     // Sample bg color
-    const bgColor = range.getCell(1, 2).getBackground();
-    for (let row = 0; row < range.getNumRows(); row++) {
-        for (let col = 0; col < range.getNumColumns(); col++) {
-            const cell = range.getCell(row + 1, col + 1);
-            if (col == 0) {
-                // Name of stage, preserve bg color
-                backgrounds[row][col] = cell.getBackground();
-            } else if (cell.isBlank()) {
-                backgrounds[row][col] = '';
-            } else {
-                backgrounds[row][col] = bgColor;
+    const bgColor = ctx.bgColors[0][1];
+    for (let row = 0; row < ctx.range.getNumRows(); row++) {
+        for (let col = 0; col < ctx.range.getNumColumns(); col++) {
+            // Preserve color of first column (name of stage)
+            if (col != 0) {
+                if (ctx.values[row][col] === '') {
+                    ctx.bgColors[row][col] = '';
+                } else {
+                    ctx.bgColors[row][col] = bgColor;
+                }
             }
         }
     }
 
-    range.setBackgrounds(backgrounds);
+    ctx.range.setBackgrounds(ctx.bgColors);
 }
 
 function getSubRange(range: SS.Range, row, col, numRows, numCols): SS.Range {
@@ -119,12 +131,28 @@ function getSubRange(range: SS.Range, row, col, numRows, numCols): SS.Range {
     return range.getSheet().getRange(absRow, absCol, numRows, numCols);
 }
 
-function doMerges(range: SS.Range) {
-    for (let row = 1; row < range.getNumRows(); row += 2) {
-        for (let col = 1; col <= range.getNumColumns(); col++) {
-            const below = getSubRange(range, row, col, 2, 1);
-            below.merge();
-        }
+function expandRight(ctx: Context, row: number, col: number): SS.Range {
+    let newCol = col + 1;
+    for (; newCol <= ctx.range.getNumColumns(); newCol++) {
+        if (ctx.values[row - 1][newCol - 1] !== '') break;
+    }
+
+    return getSubRange(ctx.range, row, col, 1, newCol - col);
+}
+
+function expandDown(ctx: Context, row: number, col: number): SS.Range {
+    let newRow = row + 1;
+    for (; newRow <= ctx.range.getNumRows(); newRow++) {
+        if (ctx.values[newRow - 1][col - 1] !== '') break;
+    }
+
+    return getSubRange(ctx.range, row, col, newRow - row, 1);
+}
+
+function doMerges(ctx: Context) {
+    for (let col = 1; col < ctx.range.getNumColumns(); col++) {
+        const mergeRegion = expandDown(ctx, 1, col);
+        mergeRegion.merge();
     }
 
     // const values = range.getValues();
@@ -139,23 +167,3 @@ function doMerges(range: SS.Range) {
     //     }
     // }
 }
-
-// // Returns the column of the next data, or null if the rest is blanks
-// function findNextDataInRow(values: any[][], row: number, col: number): number | null {
-//     for (let newCol = col + 1; newCol - 1 < values[row].length; newCol++) {
-//         if (values[row - 1][newCol - 1] != '') {
-//             return newCol;
-//         }
-//     }
-//     return null;
-// }
-
-// // Returns the row of the next data, or null if the rest is blanks
-// function findNextDataInCol(values: any[][], row: number, col: number): number | null {
-//     for (let newRow = row + 1; newRow - 1 < values.length; newRow++) {
-//         if (values[newRow - 1][col - 1] != '') {
-//             return newRow;
-//         }
-//     }
-//     return null;
-// }
